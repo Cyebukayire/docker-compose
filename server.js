b046e72e3dfbdbe58041e2dcdd8eacb7ea3b3116
cyebukayire <peacecyebukayire@gmail.com>
2022-03-10 10:04:19 +0200

    update server

--- server.js
+++ server.js
@@ -1,24 +1,47 @@
+// Import the postgres client
+const { Client } = require("pg");
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8080;
 
-app.get("/", async (req, res) => {
-  res.setHeader("Content-Type", "text/html");
+// Connect to our postgres database
+// These values like `root` and `postgres` will be
+// defined in our `docker-compose-yml` file
+const client = new Client({
+  password: "root",
+  user: "root",
+  host: "postgres",
+});
+
+
+// Serves a folder called `public` that we will create
+app.use(express.static("public"));
+
+// When a GET request is made to /employees
+// Our app will return an array with a list of all
+// employees including name and title
+// this data is defined in our `database-seed.sql` file
+app.get("/employees", async (req, res) => {
+  const results = await client
+    .query("SELECT * FROM employees")
+    .then((payload) => {
+      return payload.rows;
+    })
+    .catch(() => {
+      throw new Error("Query failed");
+    });
+  res.setHeader("Content-Type", "application/json");
   res.status(200);
-  res.send("<h1>Yes, Docker-Compose</h1>");
+  res.send(JSON.stringify(results));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+// Our app must connect to the database before it starts, so
+// we wrap this in an IIFE (Google it) so that we can wait
+// asynchronously for the database connection to establish before listening
+(async () => {
+  await client.connect();
 
-const myPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("good");
-  }, 300);
-  reject("bad");
-});
-
-myPromise.then(() => {
-  console.log("this will never run");
-});
+  app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+  });
+})();
