c939b6152ef4039a07c43ef2d5a194d2b24d243e
cyebukayire <peacecyebukayire@gmail.com>
2022-03-10 10:34:12 +0200

    add docker-compose

--- database-seed.sql
+++ database-seed.sql
@@ -7,6 +7,6 @@ CREATE TABLE employees
 );
 
 INSERT INTO employees(name, title) VALUES
- ('Meadow Crystalfreak ', 'Head of Operations'),
- ('Buddy-Ray Perceptor', 'DevRel'),
- ('Prince Flitterbell', 'Marketing Guru');
+ ('Mahoro Peace', 'Content Creator'),
+ ('Kobusinge Shallon', 'Minister of Education'),
+ ('Muhodari Sage', 'Full-stack Developer');
